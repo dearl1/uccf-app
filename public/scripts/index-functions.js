41e7c5b5cf4edd9f83c412ad5ada03d947e666b0
dearl1 <59830842+dearl1@users.noreply.github.com>
2026-01-31 23:25:21 +0000

    add social links

--- public/scripts/index-functions.js
+++ public/scripts/index-functions.js
@@ -17,6 +17,59 @@ window.onload = async function () {
 
 };
 
+function addSocialLinks(item, linksTd) {
+    if (item.url) {
+      const urlLink = document.createElement("a");
+      urlLink.href = item.url;
+      urlLink.target = "_blank";
+      urlLink.rel = "noopener noreferrer";
+      urlLink.title = "Open link";
+      urlLink.innerHTML = `<i class="fa-solid fa-arrow-up-right-from-square"></i>`;
+      linksTd.appendChild(urlLink);
+    }
+
+    if (item.website) {
+      const websiteLink = document.createElement("a");
+      websiteLink.href = item.website;
+      websiteLink.target = "_blank";
+      websiteLink.rel = "noopener noreferrer";
+      websiteLink.title = "Visit website";
+      websiteLink.innerHTML = `<i class="fa-solid fa-globe"></i>`;
+      linksTd.appendChild(websiteLink);
+    }
+
+    if (item.facebook) {
+      const fb = document.createElement("a");
+      fb.href = item.facebook;
+      fb.target = "_blank";
+      fb.rel = "noopener noreferrer";
+      fb.title = "Facebook";
+      fb.innerHTML = `<i class="fa-brands fa-facebook"></i>`;
+      linksTd.appendChild(fb);
+    }
+
+    if (item.twitter) {
+      const tw = document.createElement("a");
+      tw.href = item.twitter;
+      tw.target = "_blank";
+      tw.rel = "noopener noreferrer";
+      tw.title = "Twitter / X";
+      tw.innerHTML = `<i class="fa-brands fa-x-twitter"></i>`;
+      linksTd.appendChild(tw);
+    }
+
+    if (item.instagram) {
+      const ig = document.createElement("a");
+      ig.href = item.instagram;
+      ig.target = "_blank";
+      ig.rel = "noopener noreferrer";
+      ig.title = "Instagram";
+      ig.innerHTML = `<i class="fa-brands fa-instagram"></i>`;
+      linksTd.appendChild(ig);
+    }
+  
+}
+
 
 // Function to display the list of CU items
 function displayCUList(items) {
@@ -38,10 +91,16 @@ function displayCUList(items) {
     const campusTd = document.createElement("td");
     campusTd.textContent = item.campus ?? "-";
 
+    // Links
+    const linksTd = document.createElement("td");
+    linksTd.className = "links-cell";
+
+    addSocialLinks(item, linksTd);
 
     tr.appendChild(nameTd);
     tr.appendChild(abbrevTd);
     tr.appendChild(campusTd);
+    tr.appendChild(linksTd);
 
     if (item.full_name) {
       tr.title = item.full_name;
